961e58e0bd8c44b15ca072571f7d60925ea4fdc2
julia <git.ts@trainwit.ch>
2025-08-19 14:58:35 +1000

    booting multikernel changes (doesnn't work though)
    Signed-off-by: julia <git.ts@trainwit.ch>

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+export PATH=$PATH:$HOME/code/ts/machine_queue
+
+cd microkit
+
+python3 build_sdk.py --sel4="../seL4" --boards odroidc4_multikernel --configs debug --skip-docs --skip-tar
+python3 dev_build.py --rebuild --example hello --board odroidc4_multikernel
+
+mq.sh run -s odroidc4_pool -f ./tmp_build/loader.img -c "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
