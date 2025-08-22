8239481a2f850d9f482cdd20108a6822290ee8bd
julia <git.ts@trainwit.ch>
2025-08-22 11:24:01 +1000

    update seL4/microkit according to Freya's latest code
    Signed-off-by: julia <git.ts@trainwit.ch>

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ export PATH=$PATH:$HOME/code/ts/machine_queue
 
 cd microkit
 
-python3 build_sdk.py --sel4="../seL4" --boards odroidc4_multikernel --configs debug --skip-docs --skip-tar
+python3 build_sdk.py --sel4="../seL4" --boards odroidc4_multikernel,odroidc4_multikernel_1,odroidc4_multikernel_2 --configs debug --skip-docs --skip-tar
 python3 dev_build.py --rebuild --example hello --board odroidc4_multikernel
 
 mq.sh run -s odroidc4_pool -f ./tmp_build/loader.img -c "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
